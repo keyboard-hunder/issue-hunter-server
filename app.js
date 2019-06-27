5f621d98ab190fd3c87304f796613f1844352965
김지은 <kimjieun@gimjieuns-MacBook.local>
2019-06-27 16:24:39 +0900

    feat: get issue number for a pull request

--- app.js
+++ app.js
@@ -16,10 +16,23 @@ app.post('/', function (req, res) {
     if(status == 'closed' && body.pull_request.merged == true) {    // if closed and merged
         console.log("solved!"); // solvd
         const solvedBy = body.pull_request.user.login;
-        // call issueHunter method solve()
+        const commitMsg = body.pull_request.body;
+
+        let closePattern = /(c|C)loses\s+#[1-9][0-9]* /i;
+        let found = commitMsg.match(closePattern);
+        let issueNumber;
+        if (!found) {
+            console.log("This pull request does not close any issue.");
+        } else {
+            let issueNumPattern = /#[1-9][0-9]*/;
+            found = found[0].match(issueNumPattern);
+            issueNumber = found[0][1];
+            console.log(issueNumber);
+        }
     }
+
 });
 
 app.listen(3000, function () {
-  console.log('listening on port 3000!');
+    console.log('listening on port 3000!');
 });
